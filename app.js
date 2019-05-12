b180296f816dec98fa78db2c39a786279510ab72
JorgeAntrax <spicer_94@hotmail.com>
2019-05-12 16:00:28 -0600

    version 1 lista

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // app js here
+// 
+let $containerSlider = document.querySelector('.containerSlider');
+let $label = document.querySelector('.subtitle.cta-label');
+let $cont = 0;
+setInterval(() => {
+	$cont++;
+	if($cont < 5) {
+		$containerSlider.style.marginLeft = `-${$cont * 100}%`;
+	}else {
+		$cont = 0;
+		$containerSlider.style.marginLeft = `${$cont * 100}%`;
+	}
+
+	switch($cont) {
+		case 0:
+			$label.textContent = 'Hablemos sobre tu proyecto';
+		break;
+		case 1:
+			$label.textContent = '¿Deseas saber porque?';
+		break;
+		case 2:
+			$label.textContent = '¿Quieres saber como?';
+		break;
+		case 3:
+			$label.textContent = '¿Quieres que tu negocio sea exitoso?';
+		break;
+		case 4:
+			$label.textContent = '¿Corporativo no debe de ser sinonimo de aburrido, te explico cómo?';
+		break;
+	}
+}, 8000);
---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const br = browserSync.create();
+const sass        = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ()=>{
+    br.init({
+        server: "./"
+    });
+});
+
+gulp.task('watch', () => {
+    gulp.watch('style.scss', gulp.series('sass'));
+    gulp.watch("*.html").on('change', br.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', () => {
+    return gulp.src("./*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./"))
+        .pipe(br.stream());
+});
+
+gulp.task('default', gulp.parallel('watch','serve'));
