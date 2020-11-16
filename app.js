ee78d0be8793d1bc8bdd07292792b5d262ad5276
JorgeAntrax <spicer_94@hotmail.com>
2020-11-16 16:27:41 -0700

    update CV 2020

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // app js here
 // 
 let $containerSlider = document.querySelector('.containerSlider');
-let $label = document.querySelector('.subtitle.cta-label');
+let $label = document.querySelector('.cta-label');
 let $cont = 0;
 setInterval(() => {
 	$cont++;
@@ -29,4 +29,61 @@ setInterval(() => {
 			$label.textContent = '¿Corporativo no debe de ser sinonimo de aburrido, te explico cómo?';
 		break;
 	}
-}, 8000);
+}, 5000);
+
+const $app = new Vue({
+  el: '#root',
+  name: 'Landing',
+  data: {
+    motive: {
+      el: null,
+      timer: 5000,
+      current: 0,
+      limit: 2,
+      slides: [
+								{
+										text: 'Debes subir un escalon a la vez, asi es como se logra hasta el obstáculo más dificil',
+										author: 'Leo Quintana'
+								},
+        {
+          text: 'Sólo podremos tomar decisiones acertadas si sabemos cómo analizar e interpretar los datos',
+          author: 'Avinash Kaushik'
+        },
+        {
+          text: 'Cualquier tecnología no comprendida por el hombre es indistinguible de la magia',
+          author: 'Arthur C. Clark'
+        }
+      ]
+    }
+  },
+  methods: {
+    toogleMotive: function (e) {
+      this.motiveMotion((e.target.dataset.key * 1));
+      e.target.siblings().forEach($item => $item.removeClass('active'));
+      e.target.classList.add('active');
+    },
+    motiveMotion: function(cont, autoplay = false) {
+      if(!this.motive.el) this.motive.el = document.getElementById('ui_motive_slider');
+      if(autoplay) cont = (cont < this.motive.limit) ? cont + 1 : 0;
+      this.motive.el.scrollLeft = cont * this.motive.el.clientWidth;
+      this.motive.current = cont;
+
+      if(autoplay) {
+        document.querySelectorAll('.dot[data-key]').forEach($item => {
+          if($item.dataset.key != cont) {
+            $item.classList.remove('active');
+          }else{
+            $item.classList.add('active');
+          }
+        });
+      }
+    }
+  }
+});
+
+const motion = () => {
+  $app.motiveMotion($app.motive.current, true);
+  setTimeout(motion, $app.motive.timer);
+};
+setTimeout(motion, $app.motive.timer);
+setTimeout(() => document.querySelector('html').classList.add('loaded'), 1500);
