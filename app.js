4678f07cd68acec267989470b2eb66050c3de6c4
JorgeAntrax <spicer_94@hotmail.com>
2021-02-04 22:25:16 -0700

    add angular experience

--- app.js
+++ app.js
@@ -3,33 +3,13 @@
 let $containerSlider = document.querySelector('.containerSlider');
 let $label = document.querySelector('.cta-label');
 let $cont = 0;
-setInterval(() => {
-	$cont++;
-	if($cont < 5) {
-		$containerSlider.style.marginLeft = `-${$cont * 100}%`;
-	}else {
-		$cont = 0;
-		$containerSlider.style.marginLeft = `${$cont * 100}%`;
-	}
 
-	switch($cont) {
-		case 0:
-			$label.textContent = 'Hablemos sobre tu proyecto';
-		break;
-		case 1:
-			$label.textContent = '¿Deseas saber porque?';
-		break;
-		case 2:
-			$label.textContent = '¿Quieres saber como?';
-		break;
-		case 3:
-			$label.textContent = '¿Quieres que tu negocio sea exitoso?';
-		break;
-		case 4:
-			$label.textContent = '¿Corporativo no debe de ser sinonimo de aburrido, te explico cómo?';
-		break;
-	}
-}, 5000);
+function initSlide() {
+  $cont = $cont < 5 ? $cont+1 : 0;
+  $containerSlider.scrollLeft = $cont * $containerSlider.clientWidth;
+  setInterval(() => initSlide, 5000);
+}
+setInterval(() => initSlide, 5000);
 
 const $app = new Vue({
   el: '#root',
